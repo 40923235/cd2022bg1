8af6621cb6629cdce1c796bbc8376a27b1d9ac41
40923205 <40923205@gm.nfu.edu.tw>
2022-03-16 22:45:42 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 1233', 'tags': '', 'url': 'About.html'}]};
